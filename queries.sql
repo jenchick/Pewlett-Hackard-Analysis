1b339fd2b2a243ec639b269f8c66f8e32a0e8cfa
jenchick <jchickow@gmail.com>
2022-06-06 16:11:22 -0500

    Finish Module 7

--- queries.sql
+++ queries.sql
@@ -158,3 +158,90 @@ ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
 
 SELECT * FROM current_emp
+
+--Employee count by department number
+SELECT COUNT (ce.emp_no), de.dept_no
+INTO retire_dept_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Module 7.3.5 List 1 Employee Information
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+--Module 7.3.5 List 2 Management
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	INNER JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	INNER JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+
+--Module 7.3.5 List 3 Department Retirees
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no);
+		
+--Module 7.3.6 Create a Tailored List
+--Sales Team Retirement Info
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_dept_retire
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE (d.dept_name = 'Sales')
+
+SELECT * FROM sales_dept_retire
+
+--Sales & Development Team Retirement Info
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO sales_devel_dept_retire
+FROM current_emp as ce
+	INNER JOIN dept_emp as de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments as d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales','Development');
+
+SELECT * FROM sales_devel_dept_retire	
