20c9d2786b524ada4b0494ff11f3e30e0ea2cb73
jenchick <jchickow@gmail.com>
2022-06-10 11:26:34 -0500

    Finishing Module 7 Challenge

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -29,3 +29,27 @@ INTO retiring_titles
 FROM  unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC
+
+--Deliverable # 2; Mentorship Eligibility
+DROP TABLE mentorship_eligibility CASCADE;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+--Current Employess only:
+WHERE  de.to_date = '9999-01-01'
+--filter by bday
+AND birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY emp_no;
+
+SELECT * from mentorship_eligibility
